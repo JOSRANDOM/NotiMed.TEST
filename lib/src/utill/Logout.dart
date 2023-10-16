1b3ac8a7d13116ef62e5a00988bcb2b38ab20805
JOSRANDOM <137971696+JOSRANDOM@users.noreply.github.com>
2023-10-16 11:15:33 -0500

    correcciones menores

--- lib/src/utill/Logout.dart
+++ lib/src/utill/Logout.dart
@@ -2,6 +2,7 @@
 
 import 'package:app_notificador/src/services/provider.dart';
 import 'package:app_notificador/src/session/login.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 //import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 //import 'package:flutter_cache_manager/flutter_cache_manager.dart';
@@ -12,24 +13,23 @@ void logout(BuildContext context) async {
   try {
     SharedPreferences prefs = await SharedPreferences.getInstance();
 
-        // Eliminar todos los datos guardados en SharedPreferences
+    // Eliminar todos los datos guardados en SharedPreferences
     await prefs.clear();
-
-    await Future.wait([
-      prefs.remove(
-          'tokenFB'), // Elimina el token de Firebase Messaging u otros datos de sesión
-      // Puedes agregar más preferencias relacionadas con la sesión aquí si es necesario
-    ]);
 
     context.read<LoginProvider>().setLoginData(null);
 
+    // Elimina el token de Firebase Messaging u otros datos de sesión específicos
+    // Asegúrate de que la clave sea la misma que se usó para guardar el token durante el inicio de sesión.
+    await FirebaseMessaging.instance.deleteToken();
+
     Navigator.popUntil(context, (route) => route.isFirst);
 
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (context) => const LoginPage()),
-        );
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => const LoginPage()),
+    );
   } catch (e) {
     print("Error during logout: $e");
   }
 }
+
